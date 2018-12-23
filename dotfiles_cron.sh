dad275f3e7e5d49c0165fa67d328439343169bbd
lopun <ujin43255252@gmail.com>
2018-12-23 12:55:16 +0900

    Copying dotfiles cron added

---
+++ dotfiles_cron.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ~/.vimrc ~/dotfiles/.vimrc
+cp ~/.zshrc ~/dotfiles/.zshrc
+cp -R ~/.vim_runtime/ ~/dotfiles/.vim_runtime
+cp ~/.gitconfig ~/dotfiles/.gitconfig
+cp ~/Brewfile ~/dotfiles/Brewfile
