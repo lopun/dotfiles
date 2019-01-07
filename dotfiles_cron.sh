40ae891c08010da95e2254208d7db46406bdb737
lopun <ujin43255252@gmail.com>
2019-01-07 15:25:41 +0900

    Tmux conf file added

--- dotfiles_cron.sh
+++ dotfiles_cron.sh
@@ -2,6 +2,7 @@
 
 cp ~/.vimrc ~/dotfiles/.vimrc
 cp ~/.zshrc ~/dotfiles/.zshrc
+cp ~/.tmux.conf ~/dotfiles/.tmux.conf
 cp -R ~/.vim_runtime ./
 cp ~/.gitconfig ~/dotfiles/.gitconfig
 cp ~/Brewfile ~/dotfiles/Brewfile
